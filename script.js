7c3f2d3745e579fb174a188b5d6759363d1801ee
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-07-20 10:34:00 +0000

    update

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     // console.log("DOM fully loaded and parsed");
     const form = document.getElementById ('registration-form');
+
+    const feedbackDiv = document.getElementById ('form-feedback');
+
   });
+
+function loaded() {
+    form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            // this.id will show "form"
+            alert(this.id);
+            alertGrade(this.id);},
+            false);
+}
