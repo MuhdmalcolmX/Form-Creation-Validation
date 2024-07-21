bdd1f1b2507586509e18807a9d1efef8a342e00c
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-07-21 03:07:01 +0000

    added fetch-data.html fetch-data.css fetch-data.js files

---
+++ fetch-data.js
@@ -0,0 +1,16 @@
+async function fetchUserData() {
+    try {
+      const apiUrl  = await fetch('https://jsonplaceholder.typicode.com/users');
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json(); 
+      console.log('Success:', data); 
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  fetchUserData();
